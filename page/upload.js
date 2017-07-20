525f172f7718b018fb44063d01687398ac3a09e2
gonnavis <gonnavis@163.com>
2017-07-20 09:39:00 +0800

    a

--- page/upload.js
+++ page/upload.js
@@ -9,7 +9,27 @@ ctrl
 		$s.m={};
 		var m=$s.m;
 
-
+	// cropper
+		m.cropper={};
+		m.cropper.select_photo=function(){
+			var img=document.createElement('img');
+			img.src=URL.createObjectURL(input.files[0]);
+			img.onload=function(){
+				m.cropper.cropper=new Cropper(img,{
+					// aspectRatio: 16 / 9,
+					crop: function(e) {
+					},
+					ready:function(){
+						m.cropper.cropper.setCropBoxData({
+							left:0,
+							right:0,
+							width:window.innerWidth,
+							height:window.innerWidth,
+						})
+					}
+				});
+			}
+		}
 
 
 	
