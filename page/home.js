e2f262219ec014bd3b6f27742ed0e24301a11d2e
gonnavis <gonnavis@163.com>
2017-08-03 12:04:16 +0800

    new page home

--- page/home.js
+++ page/home.js
@@ -9,8 +9,84 @@ ctrl
 		$s.m={};
 		var m=$s.m;
 
+		m.anim_head_max=44;
+		m.anim_head_index=0;
+		m.anim_head_interval;
 
+		m.anim_band_max=44;
+		m.anim_band_index=0;
+		m.anim_band_interval;
 
+		m.anim_logo_max=34;
+		m.anim_logo_index=0;
+		m.anim_logo_interval;
+
+	// $on
+        $s.$on('$destroy', function() {
+          $s.anim_head_stop();
+          $s.anim_band_stop();
+          $s.anim_logo_stop();
+        });
+
+	// fn
+		$s.init=function(){
+			$s.anim_head_start();
+			$s.anim_band_start();
+			$s.anim_logo_start();
+		}
+
+		$s.pad2=function(int){
+			var str='0'+int;
+			return str.slice(-2);
+		}
+
+		$s.anim_head_start=function(){
+			m.anim_head_interval=$interval(function(){
+				m.anim_head_index++;
+				if(m.anim_head_index>m.anim_head_max){
+					m.anim_head_index=0;
+				}
+			},66.6)
+		}
+		$s.anim_head_stop=function(){
+			if(ng.isDefined(m.anim_head_interval)){
+				$interval.cancel(m.anim_head_interval);
+				m.anim_head_interval=undefined;
+			}
+		}
+
+		$s.anim_band_start=function(){
+			m.anim_band_interval=$interval(function(){
+				m.anim_band_index++;
+				if(m.anim_band_index>m.anim_band_max){
+					m.anim_band_index=0;
+				}
+			},66.6)
+		}
+		$s.anim_band_stop=function(){
+			if(ng.isDefined(m.anim_band_interval)){
+				$interval.cancel(m.anim_band_interval);
+				m.anim_band_interval=undefined;
+			}
+		}
+
+		$s.anim_logo_start=function(){
+			m.anim_logo_interval=$interval(function(){
+				m.anim_logo_index++;
+				if(m.anim_logo_index>m.anim_logo_max){
+					m.anim_logo_index=0;
+				}
+			},66.6)
+		}
+		$s.anim_logo_stop=function(){
+			if(ng.isDefined(m.anim_logo_interval)){
+				$interval.cancel(m.anim_logo_interval);
+				m.anim_logo_interval=undefined;
+			}
+		}
+
+	// init
+		$s.init();
 
 	
 })
